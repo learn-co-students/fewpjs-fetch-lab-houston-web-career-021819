46766c7c0da4f3b6794b1444ba8c0c4035a06e3c
josieyao <yao.josephine@yahoo.com>
2019-07-03 22:57:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   fetchBooks()
 })
